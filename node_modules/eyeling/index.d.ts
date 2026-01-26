5e547b60256057b6cfa30854c1808d1627ab79bf
william-vw <wvw@Williams-MBP.lan>
2026-01-26 07:17:54 -0500

    allinone

---
+++ node_modules/eyeling/index.d.ts
@@ -0,0 +1,3 @@
+declare module 'eyeling' {  
+    export function reason(opts:any,input: string): string;
+}
--- util.py
+++ util.py
@@ -2,9 +2,15 @@ import os, subprocess, re, multiprocess
 from rdflib import Namespace, Literal, Graph, BNode, RDF, RDFS, XSD, URIRef
 from rdflib.collection import Collection
 
+import time
+
 def run(cmd, printerr=False):    
+    start_time = time.perf_counter()
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, error = [ b.decode('UTF-8') for b in process.communicate() ]
+    end_time = time.perf_counter()
+    
+    print("time:", (end_time - start_time))
     
     if error.strip() != "" and printerr:
         print("error:", error)
